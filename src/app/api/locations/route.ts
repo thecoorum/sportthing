585c139451ce1ee30b094c7140ce2b4f77daaab6
Yaroslav <coorum.y@gmail.com>
2023-10-04 22:51:56 +0300

    Update locations API to support paging and count

--- src/app/api/locations/route.ts
+++ src/app/api/locations/route.ts
@@ -1,24 +1,44 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import { parse } from "url";
+
 import { verifyInitData } from "@/utils";
 import { supabase } from "@/supabase";
 
 export const GET = async (req: NextRequest) => {
   const tgWebAppData = req.headers.get("Web-App-Data");
 
+  const { query } = parse(req.url, true);
+
   const { success } = verifyInitData(tgWebAppData);
 
   if (success) {
     try {
-      const { data: locations, error } = await supabase
+      let queryBuilder = supabase
         .from("locations")
-        .select("*");
+        .select("*", { count: "exact" });
+
+      if (query.page && query.per) {
+        const from = (Number(query.page) - 1) * Number(query.per);
+        const to = from + Number(query.per);
+
+        queryBuilder = queryBuilder.range(from, to);
+      } else if (query.per) {
+        queryBuilder = queryBuilder.limit(Number(query.per));
+      } else if (query.page) {
+        const from = (Number(query.page) - 1) * 10;
+        const to = from + 10;
+
+        queryBuilder = queryBuilder.range(from, to);
+      }
+
+      const { data: locations, count, error } = await queryBuilder;
 
       if (error) {
         return NextResponse.json({ error }, { status: 400 });
       }
 
-      return NextResponse.json({ locations }, { status: 200 });
+      return NextResponse.json({ locations, count }, { status: 200 });
     } catch (error) {
       return NextResponse.json({ error }, { status: 400 });
     }
