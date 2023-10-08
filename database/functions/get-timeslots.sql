ead0827054b3b53f3f97296cc36ec62846b9cccd
Yaroslav <coorum.y@gmail.com>
2023-10-08 12:23:21 +0300

    Adjust timeslots function to properly work with current_date

--- database/functions/get-timeslots.sql
+++ database/functions/get-timeslots.sql
@@ -50,9 +50,9 @@ begin
 
         -- Check if the timeslot is unavailable due to a booking
         select * into r_booking from bookings 
-        where bookings.employee_id = p_employee_id and bookings.booking_date = p_date 
+        where bookings.employee_id = p_employee_id and bookings.booking_date = date_trunc('day', p_date) 
         and ((r_start_time >= bookings.start_time and r_start_time < bookings.end_time) 
-        OR (r_start_time + r_activity.duration * interval '1 minute' > bookings.start_time and r_start_time + r_activity.duration * interval '1 minute' <= bookings.end_time));
+        or (r_start_time + r_activity.duration * interval '1 minute' > bookings.start_time and r_start_time + r_activity.duration * interval '1 minute' <= bookings.end_time));
 
         -- If the timeslot is not booked, return it
         if not found then
