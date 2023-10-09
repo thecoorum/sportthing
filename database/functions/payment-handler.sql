832d7f0d02e2fb6c9ad1010e8acc8023d28e92db
Yaroslav <coorum.y@gmail.com>
2023-10-09 10:53:48 +0300

    Update the handler to working solution

--- database/functions/payment-handler.sql
+++ database/functions/payment-handler.sql
@@ -1,19 +1,14 @@
-create extension if not exists pg_cron with schema extensions;
-create extension if not exists http with schema extensions;
-
-create or replace function send_telegram_message(chat_id integer)
-returns jsonb as $$
-declare
-  response jsonb;
+create
+or replace function send_telegram_message (chat_id bigint) returns void as $$
 begin
   -- Send the HTTP request to the Telegram bot
-  select http_post(
-    'https://api.telegram.org/bot<telegram-bot-token>/sendMessage',
-    '{ "chat_id": "' || chat_id || '", "text": "Your booking has been cancelled because you did not pay within 15 minutes." }',
-    'multipart/form-data'
-  ) into response;
-
-  return response;
+  perform http_post(
+    'https://api.telegram.org/bot<your-bot-token>/sendMessage',
+    jsonb_build_object(
+      'chat_id', chat_id,
+      'text', 'Your booking has been cancelled because you did not pay within 15 minutes.'
+    )
+  );
 end;
 $$ language plpgsql;
 
@@ -28,8 +23,8 @@ begin
     -- Calculate the age of the booking
     booking_age := now() - booking_row.created_at;
 
-    -- If the booking is more than 5 minutes old, cancel it and send a message to the Telegram bot
-    if booking_age > interval '5 minutes' then
+    -- If the booking is more than 15 minutes old, cancel it and send a message to the Telegram bot
+    if booking_age > interval '15 minutes' then
       update bookings set status = 'cancelled', updated_at = now() where id = booking_row.id;
 
       perform send_telegram_message(booking_row.user_id);
@@ -37,16 +32,17 @@ begin
 
     perform pg_sleep(1);
   end loop;
+
+  return;
 end;
 $$ language plpgsql;
 
--- If you are going to change the schedule, you need to unschedule the old one first
-select cron.unschedule('check_booking_status');
+select cron.unschedule('handle-stale-bookings');
 
 select cron.schedule(
-  'check_booking_status',
+  'handle-stale-bookings',
   '*/2 * * * *',
   $$
-    perform check_booking_status();
+    select check_booking_status()
   $$
 );
